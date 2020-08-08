7d5491ab7196608e190bedb034868dafbd2177ee
R3V0LVER <chavacoliz888@gmail.com>
2020-08-08 14:04:16 -0500

    the WHILE update

--- reeee.py
+++ reeee.py
@@ -43,16 +43,16 @@ while (True):
         if(min_x <= centro_x <= max_x) & (min_y <= centro_y <= max_y):
             color = (0, 0, 255)
 
-        if centro_x > 340 :
+        while centro_x > 340 :
             print('mover a la derecha')
 
-        if centro_x < 300 :
+        while centro_x < 300 :
             print('mover a la izquiera')
 
-        if centro_y < 220 :
+        while centro_y < 220 :
             print('mover para abajo')
 
-        if centro_y > 260 :
+        while centro_y > 260 :
             print('mover para arriba')
 
         cv2.rectangle(frame, (x, y), (width, height), color, stroke)
